fa917dc946f0d94ce1a98326a337deb033210647
kush-prog <comedykid041@gmail.com>
2024-10-15 08:56:07 +0530

    Final commi of Frontend

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,16 +1,60 @@
 
-import React, { useState } from "react";
+import { useState, useEffect } from "react";
 import Header from "../components/Header";
-import { Box, Typography, Button } from '@mui/material';
+import { Box, Typography, Button, styled } from '@mui/material';
 import AddInvoice from "../components/AddInvoice";
+import Invoices from "../components/Invoices";
+import { getAllInvoice, deleteInvoice } from "../services/api";
 
+
+const Component = styled(Box)`
+    width: 80%;
+    margin: 50px auto;
+    & > h4 {
+        margin-bottom: 20px;
+    }
+    & > thead {
+        background-color: #000;
+    }
+    & > th {
+        color: #FFFFFF;
+        font-weight: 600;
+        font-size: 16px;
+    } 
+    & > td {
+        font-size: 16px;
+    }
+`
+
+const defaultObj = {
+    id: '',
+    vendor: '',
+    product: '',
+    amount: '',
+    date: ''
+}
 
 const Home = () => {
 
     const [addInvoice, setAddInvoice] = useState(false);
+    const [invoices, setInvoices] = useState([]);
+
+    useEffect(() => {
+        const getData = async () => {
+            const response = await getAllInvoice();
+            response && response.data && setInvoices(response.data);
+        }
+        getData();
+    }  , [addInvoice]);
 
     const toggleInvoice = () => {
         setAddInvoice(true);
+    }
+
+    const removeInvoice = async (id) => {
+        await deleteInvoice(id);
+        const updatedInvoices = invoices.filter(invoice => invoice.id !== id);
+        setInvoices(updatedInvoices);   
     }
 
     return (
@@ -26,7 +70,14 @@ const Home = () => {
                     >Add Invoice</Button>
                 }
                 
-                { addInvoice && < AddInvoice /> }
+                { addInvoice && < AddInvoice setAddInvoice={setAddInvoice}/> }
+
+                <Box>
+                    <Invoices 
+                        invoices = {invoices}
+                        removeInvoice = {removeInvoice} 
+                    />
+                </Box>
             </Box>
 
         </>
