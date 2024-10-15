29b5721f5e71ac40ea64f23eeb799a5537f043ec
kush-prog <comedykid041@gmail.com>
2024-10-15 08:56:56 +0530

    Final commit of backend server file.

--- server/src/main/java/com/invoiceprocessing/server/controller/InvoiceController.java
+++ server/src/main/java/com/invoiceprocessing/server/controller/InvoiceController.java
@@ -3,10 +3,9 @@ package com.invoiceprocessing.server.controller;
 import com.invoiceprocessing.server.model.Invoice;
 import com.invoiceprocessing.server.services.InvoiceService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @CrossOrigin
@@ -16,6 +15,17 @@ public class InvoiceController {
     InvoiceService invoiceService;
     @PostMapping("/invoice")
     public Invoice addInvoice(@RequestBody Invoice invoice){
+
         return this.invoiceService.addInvoice(invoice);
+    }
+
+    @GetMapping("/invoice")
+    public List<Invoice> getInvoices(){
+        return this.invoiceService.getInvoices();
+    }
+
+    @DeleteMapping("/invoice/{invoiceId}")
+    public Invoice deleteInvoice(@PathVariable String invoiceId){
+        return this.invoiceService.deleteInvoice(Long.parseLong(invoiceId));
     }
 }
--- server/src/main/java/com/invoiceprocessing/server/services/InvoiceServiceImpl.java
+++ server/src/main/java/com/invoiceprocessing/server/services/InvoiceServiceImpl.java
@@ -5,6 +5,8 @@ import com.invoiceprocessing.server.model.Invoice;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class InvoiceServiceImpl implements InvoiceService{
     @Autowired
@@ -12,6 +14,18 @@ public class InvoiceServiceImpl implements InvoiceService{
     @Override
     public Invoice addInvoice(Invoice invoice){
         invoiceDao.save(invoice);
+        return invoice;
+    }
+
+    @Override
+    public List<Invoice> getInvoices(){
+        return invoiceDao.findAll();
+    }
+
+    @Override
+    public Invoice deleteInvoice(long id){
+        Invoice invoice = invoiceDao.findById(id).get();
+        invoiceDao.delete(invoice);
         return invoice;
     }
 
